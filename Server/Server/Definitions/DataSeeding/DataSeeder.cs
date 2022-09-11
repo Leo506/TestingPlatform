9a4a34b28d1b9dc4b224578d3acb0e1650119984
Artem Levin <leoartemlev@yandex.ru>
2022-09-12 01:35:46 +0500

    add data seeding to user info database

--- Server/Server/Definitions/DataSeeding/DataSeeder.cs
+++ Server/Server/Definitions/DataSeeding/DataSeeder.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OpenIddict.Abstractions;
 using Server.Definitions.Database.Contexts;
+using Server.Models;
 
 namespace Server.Definitions.DataSeeding;
 
@@ -34,6 +35,29 @@ public class DataSeeder : IHostedService
             }, cancellationToken);
         }
 
+        var usersContext = scope.ServiceProvider.GetRequiredService<UsersDbContext>();
+        
+        if (usersContext.UserInfos.Any())
+            return;
+
+        var createdClient = await manager.FindByClientIdAsync("admin", cancellationToken);
+        var clientId = await manager.GetIdAsync(createdClient!, cancellationToken);
+        var admin = new UserInfo()
+        {
+            UserId = clientId!,
+            RoleId = 1
+        };
+
+        var adminRole = new RoleInfo()
+        {
+            RoleId = 1,
+            RoleName = "Admin"
+        };
+        
+        adminRole.UserInfos.Add(admin);
+
+        await usersContext.UserInfos.AddAsync(admin, cancellationToken);
+        await usersContext.RoleInfos.AddAsync(adminRole, cancellationToken);
     }
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
