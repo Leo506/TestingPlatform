dbb13538541a2451865af00092e3dc0d3732fa64
Artem Levin <leoartemlev@yandex.ru>
2022-09-19 01:39:22 +0500

    remove magic values

---
+++ Server/Server.Data/ServerResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Data;
+
+public class ServerResponse
+{
+    public int StatusCode { get; set; }
+    
+    public string? Description { get; set; }
+}
---
+++ Server/Server/Controllers/ControllerExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+
+namespace Server.Controllers;
+
+public static class ControllerExtension
+{
+    public static IActionResult StatusCode(this Controller controller, ServerResponse response)
+    {
+        return controller.StatusCode(response.StatusCode, response.Description);
+    }
+}
