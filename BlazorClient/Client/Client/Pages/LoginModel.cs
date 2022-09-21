098d94cea82f781f8c95253708ded2be53b157e2
Artem Levin <leoartemlev@yandex.ru>
2022-09-21 18:39:21 +0500

    write form for login

---
+++ BlazorClient/Client/Client/Pages/LoginModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+
+namespace Client.Pages;
+
+public class LoginModel : ComponentBase
+{
+    public LoginViewModel LoginData { get; set; }
+
+    public LoginModel()
+    {
+        LoginData = new LoginViewModel();
+    }
+
+    protected Task LoginAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class LoginViewModel
+{
+    [Microsoft.Build.Framework.Required]
+    [StringLength(50, ErrorMessage = "Too long username")]
+    public string UserName { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+}
---
+++ BlazorClient/Client/Client/TokenAuthProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Client;
+
+public class TokenAuthProvider : AuthenticationStateProvider
+{
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var identity = new ClaimsIdentity();
+        var principal = new ClaimsPrincipal(identity);
+        return new AuthenticationState(principal);
+    }
+}
