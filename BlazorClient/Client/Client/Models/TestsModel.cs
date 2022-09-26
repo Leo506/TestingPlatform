2053eade5b6bf889224205197830f11962f818a6
Artem Levin <leoartemlev@yandex.ru>
2022-09-26 17:19:14 +0500

    add test validation before sending HTTP request

--- BlazorClient/Client/Client/Models/TestsModel.cs
+++ BlazorClient/Client/Client/Models/TestsModel.cs
@@ -9,4 +9,20 @@ public class TestsModel
     
     [JsonPropertyName("questions")]
     public List<Question> Questions { get; set; } = new();
+
+    public bool IsValid()
+    {
+        foreach (var question in Questions)
+        {
+            var hash = new HashSet<string>(question.AnswersCollection.Select(a => a.Text));
+            if (hash.Count != question.AnswersCollection.Count())
+                return false;
+
+            var correctCount = question.AnswersCollection.Count(a => a.IsCorrect);
+            if (correctCount != 1)
+                return false;
+        }
+
+        return true;
+    }
 }
