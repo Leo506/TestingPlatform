37fca3d9d935186b31e91b09e7462600c3942f62
Artem Levin <leoartemlev@yandex.ru>
2022-10-03 00:32:36 +0500

    tests for GetAllTests

---
+++ BlazorClient/Client.UnitTests/TestsServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using Client.LocalStorage;
+using Client.Models;
+using Client.Services;
+using Client.Services.Interfaces;
+using Client.UnitTests.Helpers;
+using Client.ViewModels;
+using Moq;
+using Moq.Protected;
+
+namespace Client.UnitTests;
+
+public class TestsServiceTests
+{
+    [Fact]
+    public async Task GetAllTests_No_TokenModel_Returns_Empty()
+    {
+        // arrange
+        var localStorage = LocalStorageHelper.GetService(null);
+        var interceptorService = new Mock<IInterceptorService>();
+
+        var sut = new TestsService(new HttpClient()
+        {
+            BaseAddress = new Uri("https://test")
+        }, localStorage, interceptorService.Object);
+
+        // act
+        var result = await sut.GetAllTests();
+        var actual = result!.Any();
+
+        // assert
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public async Task GetAllTests_Bad_Response_Returns_Empty()
+    {
+        // arrange
+        var localStorage = LocalStorageHelper.GetService(new TokenModel());
+        var interceptorService = new Mock<IInterceptorService>();
+
+        var client = HttpClientHelper.GetBadClient();
+
+        var sut = new TestsService(client, localStorage, interceptorService.Object);
+
+        // act
+        var result = await sut.GetAllTests();
+        var actual = result!.Any();
+
+        // assert
+        Assert.False(actual);
+    }
+
+    [Fact]
+    public async Task GetAllTests_AllGood_Returns_Not_Empty()
+    {
+        // arrange
+        var localStorage = LocalStorageHelper.GetService(new TokenModel()
+        {
+            AccessToken = "1111"
+        });
+        var interceptorService = new Mock<IInterceptorService>();
+
+        var client = HttpClientHelper.GetClient<IEnumerable<TestViewModel>>(new[]
+        {
+            new TestViewModel()
+        });
+
+        var sut = new TestsService(client, localStorage, interceptorService.Object);
+
+        // act
+        var result = await sut.GetAllTests();
+        var actual = result!.Any();
+
+        // assert
+        Assert.True(actual);
+    }
+}
