96f9a5a3b9386fc8a00cca53d35e4f17c19171af
Artem Levin <leoartemlev@yandex.ru>
2022-10-04 01:43:40 +0500

    added sending result after test passed

--- BlazorClient/Client/Client/Models/ResultModel.cs
+++ BlazorClient/Client/Client/Models/ResultModel.cs
@@ -2,8 +2,9 @@
 
 public class ResultModel
 {
+    public string TestId { get; set; } = null!;
     public int QuestionCount { get; set; }
     public int CorrectAnswersCount { get; set; }
-    
-    public string ReturnUrl { get; set; }
+
+    public string ReturnUrl { get; set; } = null!;
 }
