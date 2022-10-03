15722625aee2bbac21183c9265d3e264d1d815db
Artem Levin <leoartemlev@yandex.ru>
2022-10-04 01:58:42 +0500

    created helper for db contexts

---
+++ Server/Server.UnitTests/Helpers/DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BindingFlags = System.Reflection.BindingFlags;
+
+namespace Server.UnitTests.Helpers;
+
+public static class DatabaseHelper
+{
+    public static T GetDbContext<T>() where T : DbContext
+    {
+        var options = new DbContextOptionsBuilder<T>()
+            .UseInMemoryDatabase(nameof(T))
+            .Options;
+
+        var prop = typeof(T).GetConstructor(BindingFlags.Public, new[] { typeof(DbContextOptions<T>) });
+        return (T)prop.Invoke(new [] {options});
+    }
+}
