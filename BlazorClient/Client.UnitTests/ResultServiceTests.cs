5395a8391de22e75e2576f614bdfb94887cf647d
Artem Levin <leoartemlev@yandex.ru>
2022-10-04 01:39:33 +0500

    ResultService.cs SendResultAsync tests

---
+++ BlazorClient/Client.UnitTests/ResultServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Client.Models;
+using Client.Services;
+using Client.UnitTests.Helpers;
+
+namespace Client.UnitTests;
+
+public class ResultServiceTests
+{
+    [Fact]
+    public async Task SendResultAsync_No_TokenModel_Returns_False()
+    {
+        // arrange
+        var sut = new ResultService(LocalStorageHelper.GetService(), HttpClientHelper.GetSimpleClient());
+
+        // act
+        var result = await sut.SendResultAsync(new ResultModel());
+
+        // assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task SendResultAsync_Bad_Response_Returns_False()
+    {
+        // arrange
+        var sut = new ResultService(LocalStorageHelper.GetService(new TokenModel()
+        {
+            AccessToken = "1111"
+        }), HttpClientHelper.GetBadClient());
+
+        // act
+        var result = await sut.SendResultAsync(new ResultModel());
+
+        // assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task SendResultAsync_All_Good_Returns_True()
+    {
+        // arrange
+        var sut = new ResultService(LocalStorageHelper.GetService(new TokenModel()
+        {
+            AccessToken = "1111"
+        }), HttpClientHelper.GetClient(""));
+
+        // act
+        var result = await sut.SendResultAsync(new ResultModel());
+
+        // assert
+        Assert.True(result);
+    }
+}
---
+++ BlazorClient/Client/Client/Services/ResultService.cs
@@ -0,0 +1,24 @@
+﻿using Client.LocalStorage;
+using Client.Models;
+
+namespace Client.Services;
+
+public class ResultService : RemoteServiceBase
+{
+    private readonly HttpClient _httpClient;
+    public ResultService(ILocalStorageService localStorageService, HttpClient httpClient) : base(localStorageService)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<bool> SendResultAsync(ResultModel result)
+    {
+        var messageResult = await CreateMessage(HttpMethod.Post, "/result/new/");
+        if (!messageResult.Ok)
+            return false;
+
+        var response = await _httpClient.SendAsync(messageResult.Result!);
+
+        return response.IsSuccessStatusCode;
+    }
+}
